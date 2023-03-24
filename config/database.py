b9fded75a07a8b937eb7d7c4f5780c5b071c448e
Bogdanrg <70534359+Bogdanrg@users.noreply.github.com>
2023-03-24 20:27:33 +0300

    Testing

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio
 
-MONGODB_URL = "mongodb://admin:admin@localhost:27018/fastapi"
+MONGODB_URL = "mongodb://admin:admin@mongodb:27017/fastapi"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 db = client['fastapi']
